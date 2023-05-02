85d094c95cb85012fc9dd976b77bbb9cb66eded8
MaulingMonkey <git@maulingmonkey.com>
2023-05-01 18:11:36 -0700

    drop auto impl of `nzst::*` via `thin::*`
    Even through a `libc::free` wrapper doesn't use layout,
    it might still debug-check the layout was valid for it,
    or implement optional performance optimizations like calling
    free_sized or free_aligned_sized (C23)

--- src/auto/_auto.rs
+++ src/auto/_auto.rs
@@ -1,3 +1,2 @@
 mod nzst_to_zsty;
-mod thin_to_nzst;
 mod thin_to_thin;
--- src/auto/thin_to_nzst.rs
+++
@@ -1,8 +0,0 @@
-use crate::{thin, nzst};
-use crate::{AllocNN, LayoutNZ};
-
-
-
-unsafe impl<A: thin::Free> nzst::Free for A {
-    unsafe fn free(&self, ptr: AllocNN, _layout: LayoutNZ) { unsafe { thin::Free::free(self, ptr) } }
-}
