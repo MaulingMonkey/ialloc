225816caab9308d4a99668099f8a83e359953165
MaulingMonkey <git@maulingmonkey.com>
2023-05-04 21:40:10 -0700

    impls!: generics support
    • `<..>` aren't delimiters at the token level, leads to ambiguity
    • `[...]` is a gross workaround that makes me sad... but it works (tm)

--- src/_impls.rs
+++ src/_impls.rs
@@ -19,13 +19,13 @@ pub mod prelude {
 #[macro_export] macro_rules! impls {
     () => {};
 
-    ( unsafe impl $(::)? core::alloc::GlobalAlloc for $ty:ty => $(::)? ialloc::nzst::Realloc; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? core::alloc::GlobalAlloc for $ty:ty => $(::)? ialloc::nzst::Realloc; $($tt:tt)* ) => {
 
         const _ : () = {
             use $crate::_impls::prelude::*;
             use nzst::*;
 
-            unsafe impl core::alloc::GlobalAlloc for $ty {
+            unsafe impl $(<$($gdef)*>)? core::alloc::GlobalAlloc for $ty {
                 #[track_caller] unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
                     let Ok(layout) = LayoutNZ::try_from(layout) else { return null_mut() };
                     Alloc::alloc_uninit(self, layout).map_or(null_mut(), |p| p.as_ptr().cast())
@@ -54,13 +54,13 @@ pub mod prelude {
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? core::alloc::Allocator(unstable $(1.50$(.0)?)?) for $ty:ty => $(::)? ialloc::zsty::Realloc; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? core::alloc::Allocator(unstable $(1.50$(.0)?)?) for $ty:ty => $(::)? ialloc::zsty::Realloc; $($tt:tt)* ) => {
 
         const _ : () = {
             use $crate::_impls::prelude::*;
             use zsty::*;
 
-            unsafe impl core::alloc::Allocator for $ty {
+            unsafe impl $(<$($gdef)*>)? core::alloc::Allocator for $ty {
                 #[track_caller] fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
                     let alloc = Alloc::alloc_uninit(self, layout).map_err(|_| AllocError)?;
                     NonNull::new(slice_from_raw_parts_mut(alloc.as_ptr().cast(), layout.size())).ok_or(AllocError)
@@ -95,62 +95,120 @@ pub mod prelude {
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? ialloc::nzst::Alloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+
+
+    // ... => core::ops::Deref
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::thin::Alloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
         const _ : () = {
             use $crate::_impls::prelude::*;
-            unsafe impl nzst::Alloc for $ty {
+            unsafe impl $(<$($gdef)*>)? thin::Alloc for $ty {
+                type Error = <<$ty as core::ops::Deref>::Target as thin::Alloc>::Error;
+                #[inline(always)] #[track_caller] fn alloc_uninit(&self, size: NonZeroUsize) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { thin::Alloc::alloc_uninit(&**self, size) }
+                #[inline(always)] #[track_caller] fn alloc_zeroed(&self, size: NonZeroUsize) -> Result<NonNull<            u8 >, Self::Error> { thin::Alloc::alloc_zeroed(&**self, size) }
+            }
+        };
+        $crate::impls!($($tt)*);
+    };
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::thin::Free for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+        const _ : () = {
+            use $crate::_impls::prelude::*;
+            unsafe impl $(<$($gdef)*>)? thin::Free for $ty {
+                #[inline(always)] #[track_caller] unsafe fn free(&self, ptr: NonNull<MaybeUninit<u8>>) { unsafe { thin::Free::free(&**self, ptr) } }
+            }
+        };
+        $crate::impls!($($tt)*);
+    };
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::thin::Realloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+        const _ : () = {
+            use $crate::_impls::prelude::*;
+            unsafe impl $(<$($gdef)*>)? thin::Realloc for $ty {
+                const CAN_REALLOC_ZEROED : bool = <$ty as thin::Realloc>::CAN_REALLOC_ZEROED;
+                #[inline(always)] #[track_caller] unsafe fn realloc_uninit(&self, ptr: NonNull<MaybeUninit<u8>>, new_size: NonZeroUsize) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { thin::Realloc::realloc_uninit(&**self, ptr, new_size) } }
+                #[inline(always)] #[track_caller] unsafe fn realloc_zeroed(&self, ptr: NonNull<MaybeUninit<u8>>, new_size: NonZeroUsize) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { thin::Realloc::realloc_zeroed(&**self, ptr, new_size) } }
+            }
+        };
+        $crate::impls!($($tt)*);
+    };
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::thin::SizeOf for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+        const _ : () = {
+            use $crate::_impls::prelude::*;
+            unsafe impl $(<$($gdef)*>)? thin::SizeOf for $ty {
+                #[inline(always)] #[track_caller] unsafe fn size_of(&self, ptr: NonNull<MaybeUninit<u8>>) -> Option<usize> { unsafe { thin::SizeOf::size_of(&**self, ptr) } }
+            }
+        };
+        $crate::impls!($($tt)*);
+    };
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::thin::SizeOfDebug for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+        const _ : () = {
+            use $crate::_impls::prelude::*;
+            unsafe impl $(<$($gdef)*>)? thin::SizeOfDebug for $ty {
+                #[inline(always)] #[track_caller] unsafe fn size_of(&self, ptr: NonNull<MaybeUninit<u8>>) -> Option<usize> { unsafe { thin::SizeOfDebug::size_of(&**self, ptr) } }
+            }
+        };
+        $crate::impls!($($tt)*);
+    };
+
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::nzst::Alloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+        const _ : () = {
+            use $crate::_impls::prelude::*;
+            unsafe impl $(<$($gdef)*>)? nzst::Alloc for $ty {
                 type Error = <<$ty as core::ops::Deref>::Target as nzst::Alloc>::Error;
-                #[track_caller] fn alloc_uninit(&self, layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { nzst::Alloc::alloc_uninit(&**self, layout) }
-                #[track_caller] fn alloc_zeroed(&self, layout: LayoutNZ) -> Result<NonNull<            u8 >, Self::Error> { nzst::Alloc::alloc_zeroed(&**self, layout) }
+                #[inline(always)] #[track_caller] fn alloc_uninit(&self, layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { nzst::Alloc::alloc_uninit(&**self, layout) }
+                #[inline(always)] #[track_caller] fn alloc_zeroed(&self, layout: LayoutNZ) -> Result<NonNull<            u8 >, Self::Error> { nzst::Alloc::alloc_zeroed(&**self, layout) }
             }
         };
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? ialloc::nzst::Free for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::nzst::Free for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
         const _ : () = {
             use $crate::_impls::prelude::*;
-            unsafe impl nzst::Free for $ty {
-                #[track_caller] unsafe fn free(&self, ptr: NonNull<MaybeUninit<u8>>, layout: LayoutNZ) { unsafe { nzst::Free::free(&**self, ptr, layout) } }
+            unsafe impl $(<$($gdef)*>)? nzst::Free for $ty {
+                #[inline(always)] #[track_caller] unsafe fn free(&self, ptr: NonNull<MaybeUninit<u8>>, layout: LayoutNZ) { unsafe { nzst::Free::free(&**self, ptr, layout) } }
             }
         };
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? ialloc::nzst::Realloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? ialloc::nzst::Realloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
         const _ : () = {
             use $crate::_impls::prelude::*;
-            unsafe impl nzst::Realloc for $ty {
-                #[track_caller] unsafe fn realloc_uninit(&self, ptr: NonNull<MaybeUninit<u8>>, old_layout: LayoutNZ, new_layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { nzst::Realloc::realloc_uninit(&**self, ptr, old_layout, new_layout) } }
-                #[track_caller] unsafe fn realloc_zeroed(&self, ptr: NonNull<MaybeUninit<u8>>, old_layout: LayoutNZ, new_layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { nzst::Realloc::realloc_zeroed(&**self, ptr, old_layout, new_layout) } }
+            unsafe impl $(<$($gdef)*>)? nzst::Realloc for $ty {
+                #[inline(always)] #[track_caller]  unsafe fn realloc_uninit(&self, ptr: NonNull<MaybeUninit<u8>>, old_layout: LayoutNZ, new_layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { nzst::Realloc::realloc_uninit(&**self, ptr, old_layout, new_layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn realloc_zeroed(&self, ptr: NonNull<MaybeUninit<u8>>, old_layout: LayoutNZ, new_layout: LayoutNZ) -> Result<NonNull<MaybeUninit<u8>>, Self::Error> { unsafe { nzst::Realloc::realloc_zeroed(&**self, ptr, old_layout, new_layout) } }
             }
         };
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? core::alloc::GlobalAlloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? core::alloc::GlobalAlloc for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
         const _ : () = {
             use $crate::_impls::prelude::*;
-            unsafe impl core::alloc::GlobalAlloc for $ty {
-                #[track_caller] unsafe fn alloc         (&self, layout: Layout) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::alloc(&**self, layout) } }
-                #[track_caller] unsafe fn alloc_zeroed  (&self, layout: Layout) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::alloc_zeroed(&**self, layout) } }
-                #[track_caller] unsafe fn dealloc       (&self, ptr: *mut u8, layout: Layout) { unsafe { core::alloc::GlobalAlloc::dealloc(&**self, ptr, layout) } }
-                #[track_caller] unsafe fn realloc       (&self, ptr: *mut u8, old_layout: Layout, new_size: usize) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::realloc(&**self, ptr, old_layout, new_size) } }
+            unsafe impl $(<$($gdef)*>)? core::alloc::GlobalAlloc for $ty {
+                #[inline(always)] #[track_caller]  unsafe fn alloc         (&self, layout: Layout) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::alloc(&**self, layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn alloc_zeroed  (&self, layout: Layout) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::alloc_zeroed(&**self, layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn dealloc       (&self, ptr: *mut u8, layout: Layout) { unsafe { core::alloc::GlobalAlloc::dealloc(&**self, ptr, layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn realloc       (&self, ptr: *mut u8, old_layout: Layout, new_size: usize) -> *mut u8 { unsafe { core::alloc::GlobalAlloc::realloc(&**self, ptr, old_layout, new_size) } }
             }
         };
         $crate::impls!($($tt)*);
     };
 
-    ( unsafe impl $(::)? core::alloc::Allocator(unstable $(1.50$(.0)?)?) for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
+    ( unsafe impl $([$($gdef:tt)*])? $(::)? core::alloc::Allocator(unstable $(1.50$(.0)?)?) for $ty:ty => $(::)? core::ops::Deref; $($tt:tt)* ) => {
         const _ : () = {
             use $crate::_impls::prelude::*;
-            unsafe impl core::alloc::Allocator for $ty {
-                #[track_caller] fn allocate             (&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> { core::alloc::Allocator::allocate(&**self, layout) }
-                #[track_caller] fn allocate_zeroed      (&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> { core::alloc::Allocator::allocate_zeroed(&**self, layout) }
-                #[track_caller] unsafe fn deallocate    (&self, ptr: NonNull<u8>, layout: Layout) { unsafe { core::alloc::Allocator::deallocate(&**self, ptr, layout) } }
-                #[track_caller] unsafe fn grow          (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::grow(&**self, ptr, old_layout, new_layout) } }
-                #[track_caller] unsafe fn grow_zeroed   (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::grow_zeroed(&**self, ptr, old_layout, new_layout) } }
-                #[track_caller] unsafe fn shrink        (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::shrink(&**self, ptr, old_layout, new_layout) } }
+            unsafe impl $(<$($gdef)*>)? core::alloc::Allocator for $ty {
+                #[inline(always)] #[track_caller]  fn allocate             (&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> { core::alloc::Allocator::allocate(&**self, layout) }
+                #[inline(always)] #[track_caller]  fn allocate_zeroed      (&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> { core::alloc::Allocator::allocate_zeroed(&**self, layout) }
+                #[inline(always)] #[track_caller]  unsafe fn deallocate    (&self, ptr: NonNull<u8>, layout: Layout) { unsafe { core::alloc::Allocator::deallocate(&**self, ptr, layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn grow          (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::grow(&**self, ptr, old_layout, new_layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn grow_zeroed   (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::grow_zeroed(&**self, ptr, old_layout, new_layout) } }
+                #[inline(always)] #[track_caller]  unsafe fn shrink        (&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> { unsafe { core::alloc::Allocator::shrink(&**self, ptr, old_layout, new_layout) } }
             }
         };
         $crate::impls!($($tt)*);
--- src/allocator/adapt/_adapt.rs
+++ src/allocator/adapt/_adapt.rs
@@ -2,16 +2,18 @@
 
 use crate::{*, Alignment};
 
-#[cfg(allocator_api = "1.50")] use core::alloc::AllocError;
-use core::alloc::Layout;
 use core::mem::MaybeUninit;
 use core::num::NonZeroUsize;
-use core::ptr::*;
 
 
 
 /// Adapt a [`thin`] allocator to a wider interface, `panic!`ing if more than [`thin::Alloc::MAX_ALIGN`] is requested.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)] #[repr(transparent)] pub struct PanicOverAlign<A>(pub A);
+
+impl<A> core::ops::Deref for PanicOverAlign<A> {
+    type Target = A;
+    fn deref(&self) -> &Self::Target { &self.0 }
+}
 
 impl<A: thin::Alloc> PanicOverAlign<A> {
     #[track_caller] fn layout_to_size(layout: LayoutNZ) -> NonZeroUsize {
@@ -29,26 +31,12 @@ impl<A: thin::Alloc> PanicOverAlign<A> {
 
 // thin::*
 
-unsafe impl<A: thin::Alloc> thin::Alloc for PanicOverAlign<A> {
-    type Error = A::Error;
-    #[inline(always)] #[track_caller] fn alloc_uninit(&self, size: NonZeroUsize) -> Result<AllocNN,  Self::Error> { self.0.alloc_uninit(size) }
-    #[inline(always)] #[track_caller] fn alloc_zeroed(&self, size: NonZeroUsize) -> Result<AllocNN0, Self::Error> { self.0.alloc_zeroed(size) }
-}
-
-unsafe impl<A: thin::Free> thin::Free for PanicOverAlign<A> {
-    #[inline(always)] #[track_caller] unsafe fn free(&self, ptr: AllocNN) { unsafe { self.0.free(ptr) } }
-    #[inline(always)] #[track_caller] unsafe fn free_nullable(&self, ptr: *mut MaybeUninit<u8>) { unsafe { self.0.free_nullable(ptr) } }
-}
-
-unsafe impl<A: thin::Realloc> thin::Realloc for PanicOverAlign<A> {
-    const CAN_REALLOC_ZEROED : bool = A::CAN_REALLOC_ZEROED;
-    #[inline(always)] #[track_caller] unsafe fn realloc_uninit(&self, ptr: AllocNN, new_size: NonZeroUsize) -> Result<AllocNN, Self::Error> { unsafe { self.0.realloc_uninit(ptr, new_size) } }
-    #[inline(always)] #[track_caller] unsafe fn realloc_zeroed(&self, ptr: AllocNN, new_size: NonZeroUsize) -> Result<AllocNN, Self::Error> { unsafe { self.0.realloc_zeroed(ptr, new_size) } }
-}
-
-unsafe impl<A: thin::SizeOf> thin::SizeOf for PanicOverAlign<A> {}
-unsafe impl<A: thin::SizeOfDebug> thin::SizeOfDebug for PanicOverAlign<A> {
-    unsafe fn size_of(&self, ptr: AllocNN) -> Option<usize> { unsafe { self.0.size_of(ptr) } }
+impls! {
+    unsafe impl[A: thin::Alloc      ] ialloc::thin::Alloc       for PanicOverAlign<A> => core::ops::Deref;
+    unsafe impl[A: thin::Free       ] ialloc::thin::Free        for PanicOverAlign<A> => core::ops::Deref;
+    unsafe impl[A: thin::Realloc    ] ialloc::thin::Realloc     for PanicOverAlign<A> => core::ops::Deref;
+    unsafe impl[A: thin::SizeOf     ] ialloc::thin::SizeOf      for PanicOverAlign<A> => core::ops::Deref;
+    unsafe impl[A: thin::SizeOfDebug] ialloc::thin::SizeOfDebug for PanicOverAlign<A> => core::ops::Deref;
 }
 
 
@@ -96,47 +84,10 @@ unsafe impl<A: thin::Realloc> nzst::Realloc for PanicOverAlign<A> {
 
 // core::*
 
-unsafe impl<A: thin::Realloc> core::alloc::GlobalAlloc for PanicOverAlign<A> {
-    #[track_caller] unsafe fn alloc(&self, layout: Layout) -> *mut u8 { zsty::Alloc::alloc_uninit(self, layout).map_or(null_mut(), |p| p.as_ptr().cast()) }
-    #[track_caller] unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 { zsty::Alloc::alloc_zeroed(self, layout).map_or(null_mut(), |p| p.as_ptr().cast()) }
-    #[track_caller] unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) { if let Some(ptr) = NonNull::new(ptr) { unsafe { zsty::Free::free(self, ptr.cast(), layout) } } }
-    #[track_caller] unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        let Ok(new_layout) = Layout::from_size_align(new_size, layout.align()) else { return null_mut() };
-        if let Some(ptr) = NonNull::new(ptr) {
-            unsafe { zsty::Realloc::realloc_uninit(self, ptr.cast(), layout, new_layout) }
-        } else {
-            zsty::Alloc::alloc_uninit(self, new_layout)
-        }.map_or(null_mut(), |p| p.as_ptr().cast())
-    }
+impls! {
+    unsafe impl[A: thin::Realloc] core::alloc::GlobalAlloc for PanicOverAlign<A> => ialloc::nzst::Realloc;
 }
 
-#[cfg(allocator_api = "1.50")] unsafe impl<A: thin::Realloc> core::alloc::Allocator for PanicOverAlign<A> {
-    #[track_caller] fn allocate(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let data = zsty::Alloc::alloc_uninit(self, layout).map_err(|_| AllocError)?.as_ptr().cast();
-        Ok(unsafe { NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(data, layout.size())) })
-    }
-
-    #[track_caller] fn allocate_zeroed(&self, layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let data = zsty::Alloc::alloc_zeroed(self, layout).map_err(|_| AllocError)?.as_ptr().cast();
-        Ok(unsafe { NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(data, layout.size())) })
-    }
-
-    #[track_caller] unsafe fn deallocate(&self, ptr: NonNull<u8>, layout: Layout) {
-        unsafe { zsty::Free::free(self, ptr.cast(), layout) }
-    }
-
-    #[track_caller] unsafe fn grow(&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let data = unsafe { zsty::Realloc::realloc_uninit(self, ptr.cast(), old_layout, new_layout) }.map_err(|_| AllocError)?.as_ptr().cast();
-        Ok(unsafe { NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(data, new_layout.size())) })
-    }
-
-    #[track_caller] unsafe fn grow_zeroed(&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let data = unsafe { zsty::Realloc::realloc_zeroed(self, ptr.cast(), old_layout, new_layout) }.map_err(|_| AllocError)?.as_ptr().cast();
-        Ok(unsafe { NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(data, new_layout.size())) })
-    }
-
-    #[track_caller] unsafe fn shrink(&self, ptr: NonNull<u8>, old_layout: Layout, new_layout: Layout) -> Result<NonNull<[u8]>, AllocError> {
-        let data = unsafe { zsty::Realloc::realloc_uninit(self, ptr.cast(), old_layout, new_layout) }.map_err(|_| AllocError)?.as_ptr().cast();
-        Ok(unsafe { NonNull::new_unchecked(core::ptr::slice_from_raw_parts_mut(data, new_layout.size())) })
-    }
+#[cfg(allocator_api = "1.50")] impls! {
+    unsafe impl[A: thin::Realloc] core::alloc::Allocator(unstable 1.50) for PanicOverAlign<A> => ialloc::zsty::Realloc;
 }
