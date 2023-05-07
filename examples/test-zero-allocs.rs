3c08b3cbd4ae5388623800e7dbc910c6d54bfc6e
MaulingMonkey <git@maulingmonkey.com>
2023-05-06 19:59:13 -0700

    scripts/build-thorough overhaul
    • don't attempt docs tests without default/all features
    • do still run the few non-doc unit tests that remain
    • attempt nightly everything (allocator_api)

--- examples/test-zero-allocs.rs
+++ examples/test-zero-allocs.rs
@@ -1,7 +1,8 @@
-use ialloc::{allocator::c::Malloc, boxed::ABox};
-use core::mem::MaybeUninit;
+#[cfg(not(c89))] fn main() { println!("missing feature = \"c89\"") }
+#[cfg(    c89 )] fn main() {
+    use ialloc::{allocator::c::Malloc, boxed::ABox};
+    use core::mem::MaybeUninit;
 
-fn main() {
     let a : ABox<[MaybeUninit<u32>], Malloc> = ABox::new_uninit_slice(0);
     assert_eq!(a.len(), 0);
 
--- scripts/build-thorough.cmd
+++ scripts/build-thorough.cmd
@@ -1,44 +1,70 @@
 @pushd "%~dp0.." && setlocal
 
 :: Stable
-@call :run-windows cargo test --no-default-features                                     || goto :die
-@call :run-windows cargo test --no-default-features --features alloc                    || goto :die
-@call :run-windows cargo test --no-default-features --features win32                    || goto :die
-@call :run-windows cargo test --no-default-features --features msvc                     || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc"       || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c89"   || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c99"   || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c11"   || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c17"   || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c23"   || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++98" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++03" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++11" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++14" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++17" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++20" || goto :die
-@call :run-windows cargo test --no-default-features --features "alloc win32 msvc c++23" || goto :die
-@call :run-windows cargo doc                                                            || goto :die
+@call :run-windows cargo test                                                                           || goto :die
+@call :run-windows cargo test --all-features                                                            || goto :die
+@call :run-windows cargo test --lib --no-default-features                                               || goto :die
+@call :run-windows cargo test --lib --no-default-features --features alloc                              || goto :die
+@call :run-windows cargo test --lib --no-default-features --features win32                              || goto :die
+@call :run-windows cargo test --lib --no-default-features --features msvc                               || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc"                 || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c89"             || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c99"             || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c11"             || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c17"             || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c23"             || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++98"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++03"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++11"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++14"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++17"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++20"           || goto :die
+@call :run-windows cargo test --lib --no-default-features --features "alloc win32 msvc c++23"           || goto :die
+@call :run-windows cargo doc                                                                            || goto :die
+
+:: Nightly
+@call :run-windows cargo +nightly test                                                                  || goto :die
+@call :run-windows cargo +nightly test --all-features                                                   || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features                                      || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features alloc                     || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features win32                     || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features msvc                      || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc"        || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c89"    || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c99"    || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c11"    || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c17"    || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c23"    || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++98"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++03"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++11"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++14"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++17"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++20"  || goto :die
+@call :run-windows cargo +nightly test --lib --no-default-features --features "alloc win32 msvc c++23"  || goto :die
+@call :run-windows cargo +nightly doc                                                                   || goto :die
 
 :: Stable (Linux)
-@call :run-linux   cargo test --no-default-features                                     || goto :die
-@call :run-linux   cargo test --no-default-features --features alloc                    || goto :die
-@call :run-linux   cargo test --no-default-features --features win32                    || goto :die
-@call :run-linux   cargo test --no-default-features --features msvc                     || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc"       || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c89"   || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c99"   || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c11"   || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c17"   || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c23"   || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++98" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++03" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++11" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++14" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++17" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++20" || goto :die
-@call :run-linux   cargo test --no-default-features --features "alloc win32 msvc c++23" || goto :die
-@call :run-linux   cargo doc                                                            || goto :die
+@call :run-linux   cargo test                                                                           || goto :die
+@call :run-linux   cargo test --all-features                                                            || goto :die
+@call :run-linux   cargo test --lib --no-default-features                                               || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features alloc                              || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features win32                              || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features msvc                               || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc"                 || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c89"             || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c99"             || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c11"             || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c17"             || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c23"             || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++98"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++03"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++11"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++14"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++17"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++20"           || goto :die
+@call :run-linux   cargo test --lib --no-default-features --features "alloc win32 msvc c++23"           || goto :die
+@call :run-linux   cargo doc                                                                            || goto :die
 
 :die
 @popd && endlocal && exit /b %ERRORLEVEL%
