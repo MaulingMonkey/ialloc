f0483dd33d8c7a8ad83e11bd9d4a6fd433c9de5c
MaulingMonkey <git@maulingmonkey.com>
2023-05-12 03:36:27 -0700

    &allocator::simple::FixedPoolLinearProbe // SAFETY party

--- src/allocator/simple/fixed_pool.rs
+++ src/allocator/simple/fixed_pool.rs
@@ -43,11 +43,37 @@ impl<const A: usize, const B: usize, const N: usize> FixedPoolLinearProbe<A, B, 
     pub fn new() -> Self {
         Self {
             #[allow(clippy::uninit_assumed_init)] // the generics confuse clippy, but [UnsafeCell<MaybeUninit<T>>; N] should be safe... probably!
+            // SAFETY: ⚠️ UnsafeCell<MaybeUninit<T>> is always "init" even if T isn't, ergo this should be safe?
             buffer: unsafe { MaybeUninit::uninit().assume_init() },
             state:  [(); N].map(|_| Cell::new(State::Free)), // XXX: prevents `new` from being `const`
             //state:  unsafe { MaybeUninit::zeroed().assume_init() }, // XXX: not yet stable
             next:   Cell::new(0),
         }
+    }
+
+    /// Get the index into `buffer` and `state` of `ptr`, an allocation of `buffer`.
+    /// The returned index is guaranteed valid for either (if undefined behavior hasn't already been invoked.)
+    ///
+    /// ### Safety
+    /// *   `ptr` must belong to `self` or this is straight up undefined behavior.  It need not be a *live* allocation - dead or never-allocated is permitted as well.
+    unsafe fn live_or_dead_index_of(&self, ptr: AllocNN) -> usize {
+        // SAFETY: ✔️ `ptr` should belong to `self.buffer` per documented safety precondition, ergo `offset_from` should be safe.
+        let index = unsafe { ptr.as_ptr().cast::<Element<A, B>>().offset_from(self.buffer.as_ptr()) } as usize;
+        if cfg!(debug_assertions) && index >= self.state.len() { bug::ub::invalid_ptr_for_allocator(ptr) }
+        index
+    }
+
+    /// Get a reference to the `state` corresponding to `ptr`.
+    ///
+    /// ### Safety
+    /// *   `ptr` must belong to `self` or this is straight up undefined behavior.  It need not be a *live* allocation - dead or never-allocated is permitted as well.
+    unsafe fn state_of(&self, ptr: AllocNN) -> &Cell<State> {
+        // SAFETY: ✔️ `ptr` should belong to `self` per `thin::Free::free`'s safety precondition
+        let index = unsafe { self.live_or_dead_index_of(ptr) };
+
+        // SAFETY: ✔️ `index` should be a valid index into `self.{buffer,state}`.  Since it was when `ptr` was allocated, and these buffers are fixed in size, this should be trivially true.
+        let state = unsafe { self.state.get_unchecked(index) };
+        state
     }
 }
 
@@ -66,16 +92,23 @@ impl<const A: usize, const B: usize, const N: usize> meta::Meta for &'_ FixedPoo
     const ZST_SUPPORTED : bool  = true;
 }
 
+// SAFETY: ✔️ all thin::* impls intercompatible with each other
+// SAFETY: ✔️ if `alloc_uninit` succeeds:
+//  • We must return a pointer to at least `size` bytes.  As all elements of `self.buffer` are only `B` bytes long, we reject `size` above that [1].
+//  • Allocations should have at least alignment `min(MAX_ALIGN, ...)`.  All elements of `self.buffer` have alignment `A` = `MAX_ALIGN` via `Element`'s definition including `AlignN` [2].
+//  • The bucket should not have already been allocated [3].
+//  • The allocations should remain valid for the lifetime of `Self`.  Ergo, we must implement this on a *reference* to FixedPoolLinearProbe, not a value of it.
+//
 unsafe impl<const A: usize, const B: usize, const N: usize> thin::Alloc for &'_ FixedPoolLinearProbe<A, B, N> where [(); A] : ValidAlignLessThan1GiB {
     fn alloc_uninit(&self, size: usize) -> Result<AllocNN, Self::Error> {
-        if size > B { return Err(()) }
+        if size > B { return Err(()) } // [1]
 
         let start = self.next.get();
         let mut pos = start;
         for states in [&self.state[..], &self.state[..start]] {
             while let Some(state) = states.get(pos) {
                 match state.get() {
-                    State::Free => {
+                    State::Free => { // [3]
                         state.set(State::Allocated);
                         self.next.set(pos+1);
                         return NonNull::new(self.buffer[pos].get().cast()).ok_or(())
@@ -90,29 +123,48 @@ unsafe impl<const A: usize, const B: usize, const N: usize> thin::Alloc for &'_ 
     }
 }
 
+// SAFETY: ✔️ all thin::* impls intercompatible with each other
+// SAFETY: ✔️ `free` should be safe to call on any allocated `ptr` belonging to `self`
+//
 unsafe impl<const A: usize, const B: usize, const N: usize> thin::Free for &'_ FixedPoolLinearProbe<A, B, N> where [(); A] : ValidAlignLessThan1GiB {
     unsafe fn free(&self, ptr: AllocNN) {
-        let index = unsafe { ptr.as_ptr().cast::<Element<A, B>>().offset_from(self.buffer.as_ptr()) } as usize;
-        if cfg!(debug_assertions) && index >= self.state.len() { bug::ub::invalid_ptr_for_allocator(ptr) }
-        let state = unsafe { self.state.get_unchecked(index) };
+        // SAFETY: ✔️ `ptr` should belong to `self` per `thin::Free::free`'s documented safety precondition
+        let state = unsafe { self.state_of(ptr) };
         if cfg!(debug_assertions) && state.get() == State::Free { bug::ub::freed_ptr_for_allocator(ptr) }
         state.set(State::Free);
     }
 }
 
+// SAFETY: ✔️ all thin::* impls intercompatible with each other
+// SAFETY: ✔️ if `realloc_uninit` succeeds with defined behavior:
+//  • We must return a pointer to at least `size` bytes.  As all elements of `self.buffer` are only `B` bytes long, we reject `size` above that [4].
+//  • Allocations should have at least alignment `min(MAX_ALIGN, ...)`.  All elements of `self.buffer` have the same alignment, so returning `ptr` again provides the same guarantee [5].
+//  • The bucket should have already been allocated [6].
+//  • The allocations should remain valid for the lifetime of `Self`.  Ergo, we must implement this on a *reference* to FixedPoolLinearProbe, not a value of it.
+//
 unsafe impl<const A: usize, const B: usize, const N: usize> thin::Realloc for &'_ FixedPoolLinearProbe<A, B, N> where [(); A] : ValidAlignLessThan1GiB {
     const CAN_REALLOC_ZEROED : bool = false;
 
     unsafe fn realloc_uninit(&self, ptr: AllocNN, new_size: usize) -> Result<AllocNN, Self::Error> {
-        if new_size > B { return Err(()) }
-        Ok(ptr)
+        // SAFETY: ✔️ `ptr` should belong to `self` per `thin::Realloc::realloc_uninit`'s documented safety precondition
+        if cfg!(debug_assertions) && unsafe { self.state_of(ptr) }.get() == State::Free { bug::ub::freed_ptr_for_allocator(ptr) } // [6]
+        if new_size > B { return Err(()) } // [4]
+        Ok(ptr) // [5]
     }
 
     unsafe fn realloc_zeroed(&self, ptr: AllocNN, new_size: usize) -> Result<AllocNN, Self::Error> {
+        // SAFETY: ✔️ trivially safe - always failing is allowed
         let _ = (ptr, new_size);
         Err(())
     }
 }
+
+// I could implement thin::SizeOf{,Debug}. I'd either need to:
+//  • Track the requested sizes
+//  • Zero the full slot for `thin::Alloc::alloc_zeroed` (which I don't currently do) and always return the slot size.
+// If I take the latter route, I could also:
+//  • Turn `thin::Realloc::realloc_zeroed` into a trivial success
+//    (the entire slot would be initially zeroed, ergo no new zeroing necessary)
 
 #[no_implicit_prelude] mod cleanroom {
     use crate::impls;
@@ -127,7 +179,7 @@ unsafe impl<const A: usize, const B: usize, const N: usize> thin::Realloc for &'
 
 
 
-type Element<const A : usize, const B : usize> = UnsafeCell<MaybeUninit<AlignN<A, [u8; B]>>>;
+type Element<const A : usize, const B : usize> = UnsafeCell<MaybeUninit<AlignN<A, [u8; B]>>>; // [2]
 
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)] #[repr(u8)] enum State { #[default] Free = 0, Allocated } // TODO: more states?
 //unsafe impl bytemuck::Zeroable for State {}
