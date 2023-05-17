7cefe204b898afc7b7f8791b93959b839a0635e9
MaulingMonkey <git@maulingmonkey.com>
2023-05-16 20:51:11 -0700

    convert OS X MAX_ALIGN notes to doc comments

--- src/allocator/cpp/new_delete_array_aligned.rs
+++ src/allocator/cpp/new_delete_array_aligned.rs
@@ -14,13 +14,13 @@ use core::ptr::NonNull;
 impl Meta for NewDeleteArrayAligned {
     type Error                  = ();
 
-    const MAX_ALIGN : Alignment = if cfg!(target_os = "macos") {
-        // macOS 11.7.6 20G1231 seems to provide at most 4 GiB alignment, returning e.g. 0x7fbf00000000 when 8 GiB alignment is requested
-        // https://github.com/MaulingMonkey/ialloc/actions/runs/4998062851/jobs/8953095728
-        ALIGN_MIN_4_GiB_MAX
-    } else {
-        Alignment::MAX
-    };
+    /// | Platform          | Value     |
+    /// | ------------------| ----------|
+    /// | OS X 64-bit       | 4 GiB (macOS 11.7.6 20G1231 [seems to](https://github.com/MaulingMonkey/ialloc/actions/runs/4998062851/jobs/8953095728) return only 4 GiB alignment when 8+ GiB is requested)
+    /// | Linux 64-bit      | [`Alignment::MAX`] (2<sup>63</sup> B)
+    /// | Windows 64-bit    | [`Alignment::MAX`] (2<sup>63</sup> B)
+    /// | \* 32-bit         | [`Alignment::MAX`] (2 GiB)
+    const MAX_ALIGN : Alignment = if cfg!(target_os = "macos") { ALIGN_MIN_4_GiB_MAX } else { Alignment::MAX };
 
     const MAX_SIZE  : usize     = usize::MAX;       // XXX: less in practice
     const ZST_SUPPORTED : bool  = false;            // platform behavior too inconsistent
