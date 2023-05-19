e6f2a4ff99ba79fad46ed234de06a3547e5930be
MaulingMonkey <git@maulingmonkey.com>
2023-05-19 09:18:46 -0700

    `allocator::cpp::NewDelete{,Array}` // SAFETY party 2

--- src/allocator/cpp/new_delete.rs
+++ src/allocator/cpp/new_delete.rs
@@ -25,25 +25,43 @@ impl meta::Meta for NewDelete {
     const ZST_SUPPORTED : bool = false; // works on both MSVC and Linux/Clang, no idea how standard this is however
 }
 
-// SAFETY: ✔️ all thin::* impls intercompatible with each other
+/// | Safety Item   | Description   |
+/// | --------------| --------------|
+/// | `align`       | ✔️ Validated via [`thin::test::alignment`]
+/// | `size`        | ✔️ Validated via [`thin::test::edge_case_sizes`]
+/// | `pin`         | ✔️ [`NewDelete`] is `'static` - allocations by [`::operator new(size_t, nothrow_t)`](https://en.cppreference.com/w/cpp/memory/new/operator_new) live until [`::operator delete(void*)`](https://en.cppreference.com/w/cpp/memory/new/operator_delete)ed.
+/// | `compatible`  | ✔️ [`NewDelete`] uses exclusively intercompatible un-arrayed, implicit-alignment operators from the same stdlib.
+/// | `compatible`  | ✔️ [`::operator delete(void*)`](https://en.cppreference.com/w/cpp/memory/new/operator_delete) is compatible with [`::operator new(size_t, nothrow_t)`](https://en.cppreference.com/w/cpp/memory/new/operator_new) (C++03 § 18.4.1.1 ¶ 12)
+/// | `exclusive`   | ✔️ [`::operator new(size_t, nothrow_t)`](https://en.cppreference.com/w/cpp/memory/new/operator_new) allocations are exclusive/unique
+/// | `exceptions`  | ✔️ [`::operator new(size_t, nothrow_t)`](https://en.cppreference.com/w/cpp/memory/new/operator_new) is `throw()` and will return null on error (C++03 § 18.4.1.1 ¶ 5, or `noexcept` in C++11+)
+/// | `threads`     | ⚠️ thread-unsafe stdlibs existed once upon a time.  I consider linking them in a multithreaded program defacto undefined behavior beyond the scope of this to guard against.
+/// | `zeroed`      | ✔️ Validated via [`thin::test::zeroed_alloc`]
+///
+// SAFETY: per above
 unsafe impl thin::Alloc for NewDelete {
     fn alloc_uninit(&self, size: usize) -> Result<AllocNN, Self::Error> {
-        // SAFETY: ⚠️ thread-unsafe stdlibs existed once upon a time.  I consider linking them in a multithreaded program defacto undefined behavior beyond the scope of this to guard against.
-        // SAFETY: ✔️ this "should" be safe for all `size`.  Unsound C++ stdlibs are #[test]ed for at the end of this file.
+        // SAFETY: per above
         NonNull::new(unsafe { ffi::operator_new_nothrow(size) }.cast()).ok_or(())
     }
 }
 
-// SAFETY: ✔️ all thin::* impls intercompatible with each other
+/// | Safety Item   | Description   |
+/// | --------------| --------------|
+/// | `compatible`  | ✔️ [`NewDelete`] uses exclusively intercompatible un-arrayed, implicit-alignment operators from the same stdlib.
+/// | `compatible`  | ✔️ [`::operator delete(void*)`](https://en.cppreference.com/w/cpp/memory/new/operator_delete) is compatible with [`::operator new(size_t, nothrow_t)`](https://en.cppreference.com/w/cpp/memory/new/operator_new) (C++03 § 18.4.1.1 ¶ 12)
+/// | `exceptions`  | ✔️ [`::operator delete(void*)`](https://en.cppreference.com/w/cpp/memory/new/operator_delete) is `throw()` and returns no errors (C++03 § 18.4.1.1 ¶ 10) / `noexcept` (C++11+)
+/// | `threads`     | ⚠️ thread-unsafe stdlibs existed once upon a time.  I consider linking them in a multithreaded program defacto undefined behavior beyond the scope of this to guard against.
+///
+// SAFETY: per above
 unsafe impl thin::Free for NewDelete {
     unsafe fn free_nullable(&self, ptr: *mut core::mem::MaybeUninit<u8>) {
-        // SAFETY: ⚠️ thread-unsafe stdlibs existed once upon a time.  I consider linking them in a multithreaded program defacto undefined behavior beyond the scope of this to guard against.
-        // SAFETY: ✔️ `ptr` is either `nullptr` (safe), or belongs to `self` per thin::Free::free_nullable's documented safety preconditions - and thus was allocated with `::operator new(size, nothrow)`.
+        // SAFETY: ✔️ `ptr` can be nullptr (C++03 § 18.4.1.1 ¶ 13, validated via [`thin::test::nullable`])
+        // SAFETY: ✔️ `ptr` otherwise belongs to `self` per [`thin::Free::free_nullable`]'s documented safety preconditions
         unsafe { ffi::operator_delete(ptr.cast()) };
     }
 }
 
-// SAFETY: ✔️ all thin::* impls intercompatible with each other
+// SAFETY: ✔️ default Realloc impl is soundly implemented in terms of Alloc+Free
 unsafe impl fat::Realloc for NewDelete {}
 
 #[no_implicit_prelude] mod cleanroom {
